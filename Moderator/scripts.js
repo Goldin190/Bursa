dc10b7c53418a8decfbfb72c0e8115bef5fa20c8
Maciej Nowowiejski <44382787+Goldin190@users.noreply.github.com>
2018-10-23 14:19:21 +0200

    Change on index.html
    Add tab with form on click "student-edit" button

--- Moderator/scripts.js
+++ Moderator/scripts.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
     $("[href]").removeClass("active");
-    $("button.btn-students-show").click(function(){
+    $("button.btn-students").click(function(){
          $(".tab-pane").removeClass("active show");
         $("#inf_student").addClass("active show");
     });
+    $("button.btn-students-edit").click(function(){
+        
+    });
+    
 });
+
+
+
+
+/*for(el of $(".info").children){
+            for(che of el.children){
+                che.replaceWith("<input type='text' placeholder="che.innerHTML">");
+            }
+        }
+        $elements.each(function()
+       $("button.btn-students-approve").click(function(){
+        $("button.btn-students-approve").replaceWith("<button class='btn btn-success float-right btn-students-edit'>Edytuj</button>");
+    }); 
+*/
